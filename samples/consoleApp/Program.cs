45e5356f86cf10328b232342d907561f38186c2c
Cristian Vasile <vasile.cristian@gmail.com>
2021-03-19 10:23:56 +0200

    Add sample and unittest projects.

---
+++ samples/consoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
